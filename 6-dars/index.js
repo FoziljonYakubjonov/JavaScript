6e5e2344a24972ee73866ac0651b0d68905febfe
FoziljonYakubjonov <yakubjonov404ee@gmail.com>
2025-10-22 17:43:21 +0500

    6-dars

---
+++ 6-dars/index.js
@@ -0,0 +1,28 @@
+// let a = undefined
+// let b = 5
+
+
+// console.log((a + b), typeof (a + b));
+// console.log((a * b), typeof (a + b));
+// console.log((a / b), typeof (a + b));
+// console.log((a - b), typeof (a + b));
+// NAN => not a number
+
+// Operators
+// % - qoldiqli bolish
+// (--) - -1 qilish
+// ++ - +1 qilish
+// ** - daraja
+// == - qiymatni tekshirish
+// === - qatiy tekshirish
+// >= - kicik yoki teng
+// <= - kicik yoki teng
+
+// -------------------------------------------------------------------------------------------
+
+// Logical operators
+
+// ( || )- Or - yoki.
+// || doimo trueni qidiradi.
+// // False lar -> 0, null, undefined, "", false, NaN
+// console.log(Nan || 0);
