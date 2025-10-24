b640c3365a227b603df2b8370ef2ed8a9e0fd47a
FoziljonYakubjonov <yakubjonov404ee@gmail.com>
2025-10-24 18:10:27 +0500

    7-dars

---
+++ 7-dars/index.js
@@ -0,0 +1,54 @@
+// // // && - And -> Va
+// // false 
+// // console.log(1 && true);
+// // // ! - Not -> Inkor trueni false qiladi falseni true
+// // // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // // ?? - Nullish -> 
+// // // falselar bunga true bolad faqat null, undefined false.
+// // console.log(false ?? 0);
+
+
+// //--------------------------------------------------------------------------
+
+
+// // if / if else /else /
+
+// age = prompt("Yoshingizni kiriting.")
+
+
+// // if (age >= 18) {
+// //     console.log("Ruxsat.")
+// // } else {
+// //     console.log("Mumkin emas.");
+
+// // } 
+// // Bog'chada oqiydi. age >= 3 && age <= 6
+
+// if(age >= 3 && age <= 6){
+//     console.log("Bog'chada oqiydi");
+    
+// } else if(age >= 7 && age <= 17){
+//     console.log("Maktabda oqiydi");
+    
+// }
+// else if(age >= 18 && age <= 30){
+//     console.log("Universitetda oqiydi");
+    
+// }
+// else if(age >= 31 && age <= 59){
+//     console.log("Ishlaydi.");
+    
+// }
+// else if(age >= 60 && age <=99){
+//     console.log("Nafaqaga chiqqan.");
+    
+// }
+// else if(age<= 100){
+//     console.log("R.I.P");
+    
+// }
+// else{
+//     console.log("Chaqalog'.");
+    
+// }
+
