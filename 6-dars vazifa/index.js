5c93aaa5869e9864d179002e94f1dee4ee08d66a
FoziljonYakubjonov <yakubjonov404ee@gmail.com>
2025-10-24 16:25:58 +0500

    6-vazifa

---
+++ 6-dars vazifa/index.js
@@ -0,0 +1,261 @@
+// // —  Operators +, -, *, /, %, (), =, ++, --, **
+// // —  Comparision ==, ===, <, >, <=, >=
+// // —  Logical Operations ||
+
+
+
+// // ++++++++++++++++++++++++++++++++++++++++++++
+// // let a = 10
+// // let b = 30
+// // console.log(a + b);
+
+// // let c = 20
+// // let d = "10"
+// // console.log(c + d);
+
+// // let e = null
+// // let f = 50
+// // console.log(e + f);
+
+// // let g = undefined
+// // let h = 90
+// // console.log(g + h);
+
+// // let i = "20"
+// // let j = "70"
+// // console.log(i + j);
+
+
+// // ----------- (-) --------------------
+// // let a1 = 100
+// // let b1 = "20"
+// // let c1 = null
+// // let d1 = undefined
+// // let e1 = "10"
+// // console.log(a1 - b1);
+// // console.log(a1 - c1);
+// // console.log(a1 - d1);
+// // console.log(b1 - e1);
+// // console.log("50" - 5);
+
+
+// // -------------------(*)--------------------
+// // let a2 = 10
+// // let b2 = "5"
+// // let c2 = null
+// // let d2 = "20"
+// // let e2 = undefined
+// // console.log(a2 * b2);
+// // console.log(a2 * c2);
+// // console.log(a2 * d2);
+// // console.log(a2 * e2);
+// // console.log("6" * "7");
+
+
+// // ----------(/)--------------------
+// // let a3 = 50
+// // let b3 = "10"
+// // let c3 = null
+// // // let d3 = undefined
+// // // let e3 = "25"
+// // console.log(a3 / b3);
+// // console.log(a3 / c3);
+// // console.log(a3 / d3);
+// // console.log(a3 / e3);
+// // console.log("100" / "25");
+
+
+// // ----------------------------------------------------------- (%)--------------------
+// // let a4 = 15
+// // let b4 = "4"
+// // let c4 = null
+// // let d4 = undefined
+// // let e4 = "6"
+// // console.log(a4 % b4);
+// // console.log(a4 % c4);
+// // console.log(a4 % d4);
+// // console.log(a4 % e4);
+// // console.log("20" % "6");
+
+
+// // // --------------------(())--------------------
+// // let a5 = 10
+// // let b5 = 5
+// // let c5 = 2
+// // let d5 = "3"
+// // let e5 = null
+// // console.log((a5 + b5) * c5);
+// // console.log(a5 + (b5 * c5));
+// // console.log((a5 - d5) * c5);
+// // console.log((a5 + e5) / b5);
+// // console.log((a5 + "50") + b5);
+
+
+// // // -------------------- (=)-------------------------------------
+// // let a6 = 10
+// // let b6 = 20
+// // let c6 = null
+// // let d6 = "30"
+// // let e6 = undefined
+
+// // console.log(a6); // 20
+// // console.log(b6); // "40"
+// // console.log(c6); // 100
+// // console.log(d6); // null
+// // console.log(e6); // "60"
+
+
+// // // -----------------(++)--------------------
+// // let a7 = 5
+// // let b7 = "10"
+// // let c7 = null
+// // let d7 = undefined
+// // let e7 = 0
+
+// // console.log(++a7);
+// // console.log(++b7);
+// // console.log(++c7);
+// // console.log(d7++);
+// // console.log(e7++);
+
+
+// // // ---(--)----------------------------------------------------------
+// // let a8 = 10
+// // // let b8 = "5"
+// // // let c8 = null
+// // // let d8 = undefined
+// // let e8 = "100"
+// // console.log(--a8);
+// // console.log(b8--);
+// // console.log(--c8);
+// // console.log(d8--);
+// // console.log(--e8);
+
+
+
+
+
+
+
+
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+// // (==)
+// let f1 = 10
+// let g1 = "10"
+// let h1 = null
+// let i1 = undefined
+// let j1 = 0
+
+// console.log(f1 == g1)
+// console.log(h1 == j1)
+// console.log(h1 == i1)
+// console.log(f1 == j1)
+// console.log("5" == 5)
+
+
+// // (===)
+// let f2 = 100
+// let g2 = "100"
+// let h2 = null
+// let i2 = undefined
+// let j2 = 20
+
+// console.log(f2 === g2)
+// console.log(h2 === j2)
+// console.log(h2 === i2)
+// console.log(f2 === 10)
+// console.log("5" === 5)
+
+
+// // (<)
+// let f3 = 5
+// let g3 = undefined
+// let h3 = 20
+// let i3 = "2"
+// let j3 = null
+
+// console.log(f3 < g3)
+// console.log(g3 < h3)
+// console.log(i3 < f3)
+// console.log(j3 < f3)
+// console.log("50" < "5")
+
+
+// // (>)
+// let f4 = 10
+// let g4 = "5"
+// let h4 = null
+// let i4 = "20"
+// let j4 = undefined
+
+// console.log(f4 > g4)
+// console.log(g4 > i4)
+// console.log(h4 > g4)
+// console.log(i4 > "15")
+// console.log(j4 > 0)
+
+
+// // (<=)
+// let f5 = 10
+// let g5 = "10"
+// let h5 = null
+// let i5 = 5
+// let j5 = "15"
+
+// console.log(f5 <= g5)
+// console.log(i5 <= f5)
+// console.log(h5 <= i5)
+// console.log(j5 <= f5)
+// console.log("8" <= "8")
+
+
+// // (>=)
+// let f6 = 10
+// let g6 = "10"
+// let h6 = null
+// let i6 = 0
+// let j6 = "20"
+
+// console.log(f6 >= g6)
+// console.log(i6 >= h6)
+// console.log(j6 >= f6)
+// console.log(f6 >= "15")
+// console.log("5" >= "50")
+
+
+
+
+// ------------------------------------------(||)----------------------------------------------------
+
+// let a7 = true
+// let b7 = false
+// let c7 = undefined
+// let d7 = "true"
+// let f7 = null
+
+// console.log(
+//     a7 || b7
+// );
+// console.log(
+//     c7 || d7
+// );
+// console.log(
+//     b7 || d7
+// );
+// console.log(
+//     c7 || f7
+// );
+// console.log(
+//     b7 || c7
+// );
+
