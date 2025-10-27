593780796b9f7a0df2c09b2a966af04dd56eb4f8
FoziljonYakubjonov <yakubjonov404ee@gmail.com>
2025-10-27 06:30:36 +0500

    7-dars vazifa

---
+++ 7-dars vazifa/index.js
@@ -0,0 +1,76 @@
+
+// // // let car = prompt("Mashina rusumini kiriting: ");
+// // // if (car === "Chevrolet") {
+// // //     console.log("Bu O‘zbekistonda ishlab chiqarilgan mashina.");
+// // // } else if (car === "BMW") {
+// // //     console.log("Bu Germaniya mashinasi.");
+// // // } else if (car === "Toyota") {
+// // //     console.log("Bu Yaponiya mashinasi.");
+// // // } else {
+// // //     console.log("Bunday rusum haqida ma’lumot yo‘q.");
+// // // }
+
+
+// // let aksiya = prompt("Chegirma foizini kiriting:");
+// // if (aksiya < 10) {
+// //     console.log("Kichik aksiya.");
+// // } else if (aksiya >= 10 && aksiya <= 50) {
+// //     console.log("O‘rtacha aksiya.");
+// // } else if (aksiya > 50) {
+// //     console.log("Super aksiya!");
+// // } else {
+// //     console.log("Noto‘g‘ri qiymat.");
+// // }
+
+// let narx = prompt("Mashina narxini kiriting ($):");
+// if (narx < 10000) {
+//     console.log("Bu arzon mashina.");
+// } else if (narx >= 10000 && narx <= 40000) {
+//     console.log("O‘rtacha narxdagi mashina.");
+// } else {
+//     console.log("Bu qimmat mashina.");
+// }
+
+
+// let phone = prompt("Telefon markasini kiriting:");
+// if (phone === "iPhone") {
+//     console.log("Apple kompaniyasiga tegishli.");
+// } else if (phone === "Samsung") {
+//     console.log("Janubiy Koreya mahsuloti.");
+// } else if (phone === "Xiaomi") {
+//     console.log("Xitoy kompaniyasi.");
+// } else {
+//     console.log("Noma’lum model.");
+// }
+
+
+// let baho = prompt("Bahoni kiriting:");
+// if (baho >= 90) {
+//     console.log("A’lo!");
+// } else if (baho >= 70) {
+//     console.log("Yaxshi!");
+// } else if (baho >= 50) {
+//     console.log("Qoniqarli.");
+// } else {
+//     console.log("Imtihondan o‘ta olmadi.");
+// }
+
+// let day = prompt("Hafta kunini kiriting:");
+// if (day === "shanba" || day === "yakshanba") {
+//     console.log("Dam olish kuni!");
+// } else {
+//     console.log("Ish kuni.");
+// }
+
+// let speed = prompt("Internet tezligini kiriting (Mbps):");
+// if (speed < 5) {
+//     console.log("Juda sekin.");
+// } else if (speed >= 5 && speed <= 20) {
+//     console.log("O‘rtacha tezlik.");
+// } else {
+//     console.log("Juda tez internet!");
+// }
+
+
+
+
--- 7-dars/index.js
+++ 7-dars/index.js
@@ -13,42 +13,42 @@
 
 // // if / if else /else /
 
-// age = prompt("Yoshingizni kiriting.")
+age = prompt("Yoshingizni kiriting.")
 
 
-// // if (age >= 18) {
-// //     console.log("Ruxsat.")
-// // } else {
-// //     console.log("Mumkin emas.");
+// if (age >= 18) {
+//     console.log("Ruxsat.")
+// } else {
+//     console.log("Mumkin emas.");
 
-// // } 
-// // Bog'chada oqiydi. age >= 3 && age <= 6
+// } 
+// Bog'chada oqiydi. age >= 3 && age <= 6
 
-// if(age >= 3 && age <= 6){
-//     console.log("Bog'chada oqiydi");
+if(age >= 3 && age <= 6){
+    console.log("Bog'chada oqiydi");
     
-// } else if(age >= 7 && age <= 17){
-//     console.log("Maktabda oqiydi");
+} else if(age >= 7 && age <= 17){
+    console.log("Maktabda oqiydi");
     
-// }
-// else if(age >= 18 && age <= 30){
-//     console.log("Universitetda oqiydi");
+}
+else if(age >= 18 && age <= 30){
+    console.log("Universitetda oqiydi");
     
-// }
-// else if(age >= 31 && age <= 59){
-//     console.log("Ishlaydi.");
+}
+else if(age >= 31 && age <= 59){
+    console.log("Ishlaydi.");
     
-// }
-// else if(age >= 60 && age <=99){
-//     console.log("Nafaqaga chiqqan.");
+}
+else if(age >= 60 && age <=99){
+    console.log("Nafaqaga chiqqan.");
     
-// }
-// else if(age<= 100){
-//     console.log("R.I.P");
+}
+else if(age<= 100){
+    console.log("R.I.P");
     
-// }
-// else{
-//     console.log("Chaqalog'.");
+}
+else{
+    console.log("Chaqalog'.");
     
-// }
+}
 
