1fcbcf1a1fc7c2f22bf8772e1d28447105b94994
FoziljonYakubjonov <yakubjonov404ee@gmail.com>
2025-10-29 17:41:54 +0500

    9-dars

---
+++ 9-dars/index.js
@@ -0,0 +1,26 @@
+// let data = 2
+
+// if (data % 2 === 1) console.log(
+//     "Bu son toq."
+// );
+// else if (data % 2 === 0) console.log("Juft son.");
+// else console.log(
+//     "Juft ham emas toq ham emas"
+// ); 
+
+// data % 2 === 1 
+// ? console.log(
+//     "Bu son toq."
+// ) : a % 2 === 0 ? console.log("Juft son.") : console.log(
+//     "Juft ham emas toq ham emas"
+// );
+
+// for (let n=1; n<=10; n++){
+//     if(n%2 === 1) {
+//         console.log(n ,"Bu son toq.");
+//     }
+//      else console.log(n, "Bu son juft.");
+// }
+// /////////////////////////////////////////////////////////////
+// break ozidan keyingilarni yoqotadi.
+// continue ozini qatnashtirmaydi
