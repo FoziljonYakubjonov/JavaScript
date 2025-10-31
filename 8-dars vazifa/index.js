67b31829e9855d51fd4df82ef61939a91dd64563
FoziljonYakubjonov <yakubjonov404ee@gmail.com>
2025-10-31 17:46:54 +0500

    10-dars

---
+++ 8-dars vazifa/index.js
@@ -0,0 +1,39 @@
+// let a = +prompt('a?', '');
+
+// if (a == 0) {
+//   alert(0);
+// }
+
+// if (a == 1) {
+//   alert(1);
+// }
+
+// if (a == 2 || a == 3) {
+//   alert('2,3');
+// }
+// switch (browser) {
+//   case 'Edge':
+//     alert("You've got the Edge!");
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert('Okay we support these browsers too');
+//     break;
+
+//   default:
+//     alert('We hope that this page looks ok!');
+// }
+// // let message;
+
+// // if (login == 'Employee') {
+// //   message = 'Hello';
+// // } else if (login == 'Director') {
+// //   message = 'Greetings';
+// // } else if (login == '') {
+// //   message = 'No login';
+// // } else {
+// //   message = '';
+// // }
---
+++ 9-dars vazifa/index.js
@@ -0,0 +1,11 @@
+let n;
+
+for (; true;) {
+  n = prompt("100 dan katta son kiriting:");
+
+  if (n > 100 || n === null) {
+console.log(n);
+
+    break;
+  }
+}
