6bf9c4355d3c1bc3116eec9581126d065308c965
FoziljonYakubjonov <yakubjonov404ee@gmail.com>
2025-11-03 17:41:46 +0500

    11-dars

---
+++ 11-dars/index.js
@@ -0,0 +1,25 @@
+// // Function
+// let a = 20
+// let b =30
+
+// function xisobla1(){
+
+//     let sum = a + b
+//     return sum
+// }
+// console.log(xisobla1());
+
+
+// // xisobla1()
+// // let xisobla2 = function(){
+// //     console.log("Salom.");
+    
+// // }
+// // xisobla2()
+// // let xisobla3 = ()=>{
+// //     console.log("Oxford");
+    
+// // }
+// // xisobla3()
+
+
