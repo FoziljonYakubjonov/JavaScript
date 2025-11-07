f1b4e5d5f9f7e6aa94d6502e404f9791965ea690
FoziljonYakubjonov <yakubjonov404ee@gmail.com>
2025-11-07 18:04:00 +0500

    13-dars

---
+++ 13-dars/index.js
@@ -0,0 +1,73 @@
+// // let obj = {}
+
+// // let obj2 = new Object()
+
+// // let obj3 = Object()
+
+
+
+// // let obj = {
+// //     name : "Foziljon",
+// //     age : 14
+// // }
+
+// // console.log(obj.age); -------------> STATIC
+
+// // let key = "age"
+// // console.log(obj[key]); --------------------->Dinamik
+
+// // let obj1 ={
+// //     name : "Eshmat"
+// // }
+
+// // let obj2 = Object.assign({}, obj1)
+
+// // let obj2 = structuredClone(obj1)
+// //-------------------------------------------------------------------------------------------------------------------------------------------/''_''/
+// // Loop for in
+
+// // let obj = {
+// //     name : "Eshmat",
+// //     familya : "Toshmatov",
+// //     age : 27
+
+// // }
+// // for (let key in obj){
+// //     console.log(key , obj[key]);
+
+// // }
+
+// let obj1 = {
+//     ism: "Ali",
+//     familya: "Soliyev",
+//     yosh: 17,
+//     manzil: "Toshkent"
+// }
+
+
+// let obj2 = {
+//     ism: "Soli",
+//     familya: "Nabiyev",
+//     yosh: 27,
+//     manzil: "Namangan"
+// }
+
+
+// let clnobj1 = Object.assign({}, obj1)
+
+// clnobj1.ism = "Aziz"
+
+// let clnobj2 = structuredClone(obj2)
+
+// clnobj2.ism = "Bekzod"
+
+// for (let key in clnobj1) {
+//     console.log(key, "->", clnobj1[key]);
+
+// }
+
+
+// for (let key in clnobj2) {
+//     console.log(key, "->", clnobj2[key]);
+
+// }
