9475c4792da576d91c1156fbcee62cb266aec5dd
FoziljonYakubjonov <yakubjonov404ee@gmail.com>
2025-11-12 17:37:46 +0500

    15-dars

---
+++ 15-dars/index.js
@@ -0,0 +1,15 @@
+let parent = {
+    name: "Eshmat",
+    age: "100",
+    child: {
+        name: "Toshmat",
+        age: "60",
+        subChild: {
+            name: "Gulmat",
+            age: "40",
+        }
+    }
+}
+// console.log(parent.child.subChild.name);
+
+
